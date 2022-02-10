0cabb8006700c0fdad699cfdea8b89a28244943a
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-09 20:17:05 -0500

    added assets and moved utils

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
-const path = require("path");
-
-let generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./assets/utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
